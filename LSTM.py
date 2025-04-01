7c9b408409af57ece98644737da288fa74b4aa26
Zhou-or-Joe <ZhouXuyan6@gmail.com>
2025-04-01 09:31:13 +0800

    detection model

--- LSTM.py
+++ LSTM.py
@@ -1,9 +1,11 @@
 import json
+import re
 import numpy as np
 
 from keras import Sequential, layers, optimizers, utils
 from keras_preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
+from sklearn.metrics import classification_report, confusion_matrix
 
 # Read data from JSON
 def read_data(path, title_key='headline', label_key='is_sarcastic'):
@@ -18,29 +20,46 @@ def read_data(path, title_key='headline', label_key='is_sarcastic'):
 
     return headlines[100:], labels[100:], headlines[:100], labels[:100]
 
+# Preprocess text
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r'[^\w\s]', '', text)
+    return text
+
 # Classify Sigmoid Scores
 def step(prob):
     return 0 if prob < 0.5 else 1
 
 # LSTM model for sarcasm detection
 class BinaryLSTMModel:
-    def __init__(self, vocab_size=10000, output_dim=128):
+    def __init__(self, vocab_size=10000, output_dim=128, embedding_matrix=None):
+        # nn = Sequential(
+        #     [
+        #         layers.Embedding(input_dim=vocab_size, output_dim=output_dim),
+        #         layers.Bidirectional(layers.LSTM(128, return_sequences=True)),
+        #         layers.Bidirectional(layers.LSTM(32)),
+        #         layers.Dropout(0.4),
+        #         layers.Dense(32, activation='relu'),
+        #         layers.Dropout(0.4),
+        #         layers.Dense(1, activation='sigmoid')
+        #     ]
+        # )
         nn = Sequential(
             [
-                layers.Embedding(input_dim=vocab_size, output_dim=output_dim),
+                layers.Embedding(input_dim=vocab_size, output_dim=output_dim, weights=[embedding_matrix], trainable=False) if embedding_matrix is not None else layers.Embedding(input_dim=vocab_size, output_dim=output_dim),
                 layers.Bidirectional(layers.LSTM(128, return_sequences=True)),
-                layers.Bidirectional(layers.LSTM(32)),
-                layers.Dropout(0.4),
-                layers.Dense(32, activation='relu'),
-                layers.Dropout(0.4),
+                layers.Bidirectional(layers.LSTM(64)),
+                layers.Dropout(0.5),
+                layers.Dense(64, activation='relu', kernel_regularizer='l2'),
+                layers.Dropout(0.5),
                 layers.Dense(1, activation='sigmoid')
             ]
         )
-        nn.compile(optimizer=optimizers.Adam(learning_rate=1e-5), loss='binary_crossentropy', metrics=['accuracy'])
+        nn.compile(optimizer=optimizers.Adam(learning_rate=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
 
         self.model = nn
-        self.epochs = 25
-        self.batch_size = 64
+        self.epochs = 30
+        self.batch_size = 32
     
     # Train
     def fit(self, X_train, X_labels):
@@ -55,6 +74,9 @@ def main():
 
     # Prepare data
     X_train, y_train, X_test, y_test = read_data("./Sarcasm_Headlines_Dataset.json")
+    X_train = [preprocess_text(text) for text in X_train]
+    X_test = [preprocess_text(text) for text in X_test]
+
     tokenizer = Tokenizer(num_words=10000)
     tokenizer.fit_on_texts(X_train)
     sequences = tokenizer.texts_to_sequences(X_train)
@@ -67,12 +89,26 @@ def main():
     model.fit(padded_sequences, labels)
 
     # Attempt predictions
+    # score = 0
+    # pred = model.predict(padded_test_sequences)
+    # for i in range(len(pred)):
+    #     if step(pred[i]) == y_test[i]:
+    #         score += 1
+    
+    # print(score/len(pred))
+
+    # Attempt predictions
     score = 0
     pred = model.predict(padded_test_sequences)
-    for i in range(len(pred)):
-        if step(pred[i]) == y_test[i]:
-            score += 1
-    
-    print(score/len(pred))
+    pred_labels = [step(p) for p in pred]
 
-# main()
+    for i in range(len(pred_labels)):
+        if pred_labels[i] == y_test[i]:
+            score += 1
+    print("Percentage of correct predictions: ", score/len(pred_labels))
+
+    # Evaluate model
+    print(confusion_matrix(y_test, pred_labels))
+    print(classification_report(y_test, pred_labels))
+
+main()
